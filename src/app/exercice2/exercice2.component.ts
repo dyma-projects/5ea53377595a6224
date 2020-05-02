cd189ab4b0ccb86d4c029e93f3267a4f530b4685
Fabrice DARBAS <fabrice.darbas@orange.com>
2020-05-02 09:42:24 +0200

    exercice 2

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, DoCheck,OnChanges, AfterContentChecked, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-exercice2',
   templateUrl: './exercice2.component.html',
   styleUrls: ['./exercice2.component.css']
 })
-export class Exercice2Component implements OnInit {
+export class Exercice2Component implements OnInit{
   public valeur: string;
+
+  @ViewChild('f', {static: true})
+  public el: ElementRef
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  updateValeur(){
+    console.log(this.el.nativeElement.value);
+    this.valeur=this.el.nativeElement.value;
+  }
+
+  
 
 }
